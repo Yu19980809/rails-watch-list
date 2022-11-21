0f495d4f7d7c0544fe3ba59fe4fdab26e0a14819
Guangxin Yu <1572250276@qq.com>
2022-11-21 13:59:45 +0800

    get movie by TMDB api

--- Gemfile
+++ Gemfile
@@ -7,6 +7,8 @@ gem "autoprefixer-rails"
 gem "bootstrap"
 gem "font-awesome-sass", "~> 6.1"
 gem "simple_form", github: "heartcombo/simple_form"
+gem "faker"
+gem "open-uri"
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.0.4"
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,9 +12,19 @@ puts "Finished deleting"
 
 puts "Adding new movies"
 
-Movie.create(title: "Wonder Woman 1984", overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9)
-Movie.create(title: "The Shawshank Redemption", overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison", poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: 8.7)
-Movie.create(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
-Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
+movies = JSON.parse(URI.open("https://api.themoviedb.org/3/movie/top_rated?api_key=ffbd3198075d3482fa2a18de76578c2c&language=en-US&page=1").read)
+movies = movies["results"]
+
+5.times do
+  data = movies.sample
+  movie = Movie.new
+  movie.title = data["title"]
+  movie.overview = data["overview"]
+  movie.poster_url = data["poster_path"]
+  movie.rating = data["vote_average"]
+  movie.save!
+
+  puts "Adding #{movie.id} - #{movie.title}"
+end
 
 puts "Finished adding, #{Movie.count} movie(s) have been added."
